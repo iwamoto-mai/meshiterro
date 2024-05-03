cee1d1b82382315026f3ce4686caa375b6706efc
MaiIwamoto <gng9134@gmail.com>
2024-05-03 07:58:59 +0000

    Initial Commit

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,29 @@
+class HomesController < ApplicationController
+  def top
+    
+  end
+  
+  def about
+  
+  end
+  
+  def new
+    
+  end
+  
+  def create
+   
+  end
+  
+  def index
+    
+  end
+  
+  def show
+    
+  end
+  
+  
+  
+end
+
